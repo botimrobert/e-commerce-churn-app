3bdda4d8450184e769244735bd7dac7a1f30b3c8
botimrobert <botimrobert@gmail.com>
2026-03-21 00:13:02 +0300

    Update streamlit_app.py

--- artifacts/streamlit_app.py
+++ artifacts/streamlit_app.py
@@ -13,9 +13,7 @@ st.caption("Inputs are aligned to the training schema you provided.")
 # Paths & model loading
 # =========================
 
-
-ARTIFACTS_DIR = Path.cwd() / "artifacts"
-MODEL_PATH = Path("artifacts/best model.joblib")   # <- adjust only if your file is named differently
+MODEL_PATH = Path("artifacts")/"best model.joblib"  # <- adjust only if your file is named differently
 
 @st.cache_resource(show_spinner=False)
 def load_model(path: Path):
